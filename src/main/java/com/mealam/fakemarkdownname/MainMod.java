fb836bad368fe0823238cf2d9fccff833382c48f
Aram <schonhagearam@gmail.com>
2024-12-23 00:14:59 +0100

    Added the basic Project

---
+++ src/main/java/com/mealam/fakemarkdownname/MainMod.java
@@ -0,0 +1,8 @@
+package com.mealam.fakemarkdownname;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MainMod.MODID)
+public class MainMod {
+    public static final String MODID = "fakemarkdownname";
+}
